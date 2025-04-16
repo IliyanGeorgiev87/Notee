14fffaf11675b53a8a21c1d60e51d67e78f3ba92
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-04-16 21:04:52 +0300

    Notes views as classes + css

--- Notee/notes/views.py
+++ Notee/notes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+from django.views.generic import ListView, DetailView
 
-def detail(request, pk):
-    note = Notes.objects.get(pk=pk)
-    return render(request, 'notes/notes_detail.html', {'note': note})
 # Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
