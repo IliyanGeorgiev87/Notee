4e3e64f3be5d1aeccee1e67572e76a4afdf5b23a
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-04-18 15:21:10 +0300

    form

--- Notee/notes/views.py
+++ Notee/notes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Notes
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView
 
 # Create your views here.
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text']
+    success_url = "/smart/notes/"
+
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
