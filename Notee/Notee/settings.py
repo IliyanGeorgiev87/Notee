2c2e8cbbd78eab54d84b63c2e03379bed0ed6564
IliyanGeorgiev87 <iliyangeorgiev28@itpg-varna.bg>
2025-04-18 14:29:00 +0300

    html template

--- Notee/Notee/settings.py
+++ Notee/Notee/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'Notee.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
